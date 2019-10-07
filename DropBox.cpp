c323eb6f0f11798439b55fa1d24534b2fa5ff6f2
Alexis-gc <55477404+Alexis-gc@users.noreply.github.com>
2019-10-07 15:40:09 -0500

    Add files via upload

---
+++ DropBox.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main(void){
+    int a=4;
+    int b=10;
+    int c=4;
+    float z, d= 1.1;
+    z=a+b/c*d;
+printf("Variable igual a %f#\n",z);
+
+}
